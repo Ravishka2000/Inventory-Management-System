d57931acd9752db660498122225d5a9899434287
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2022-07-22 13:07:44 +0530

    Create register page

--- InventoryManagementSystem/settings.py
+++ InventoryManagementSystem/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'dashboard',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +128,8 @@ STATIC_ROOT = (BASE_DIR/"asert/")
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
