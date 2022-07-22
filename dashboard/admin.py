53624263cc46a9aeedbe4cd7d807e4dd28ac3d0a
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2022-07-22 11:21:01 +0530

    Configure admin panel

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Product
 
-# Register your models here.
+
+# change header of the admin Dashboard
+admin.site.site_header = 'No Way Inventory Dashboard'
+
+
+# change the view of the admin dashboard tables
+class ProductAdmin(admin.ModelAdmin):
+    # define columns to display in tabular format
+    list_display = ('name', 'category', 'quantity')
+    # generate a filter section
+    list_filter = ['category']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.unregister(Group)
+
