ac6ac440227750b1016a84acd11cd278a16ceb94
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2022-07-23 21:43:14 +0530

    Top nav configure done

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,6 +10,9 @@ from .forms import ProductForm, OrderForm
 def index(request):
     orders = Order.objects.all()
     products = Product.objects.all()
+    orders_count = orders.count()
+    products_count = products.count()
+    workers_count = User.objects.all().count()
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
@@ -23,6 +26,9 @@ def index(request):
         "orders": orders,
         "form": form,
         "products": products,
+        "workers_count": workers_count,
+        "orders_count": orders_count,
+        "products_count": products_count,
     }
     return render(request, 'dashboard/index.html', context)
 
@@ -30,8 +36,14 @@ def index(request):
 @login_required
 def staff(request):
     workers = User.objects.all()
+    workers_count = workers.count()
+    orders_count = Order.objects.all().count()
+    products_count = Product.objects.all().count()
     context = {
         "workers": workers,
+        "workers_count": workers_count,
+        "orders_count": orders_count,
+        "products_count": products_count,
     }
     return render(request, 'dashboard/staff.html', context)
 
@@ -48,6 +60,9 @@ def staff_detail(request, pk):
 @login_required
 def product(request):
     items = Product.objects.all()
+    products_count = items.count()
+    workers_count = User.objects.all().count()
+    orders_count = Order.objects.all().count()
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
@@ -60,6 +75,9 @@ def product(request):
     context = {
         "items": items,
         "form": form,
+        "workers_count": workers_count,
+        "orders_count": orders_count,
+        "products_count": products_count,
     }
     return render(request, 'dashboard/product.html', context)
 
@@ -92,9 +110,14 @@ def product_update(request, pk):
 @login_required
 def order(request):
     orders = Order.objects.all()
-
+    orders_count = orders.count()
+    workers_count = User.objects.all().count()
+    products_count = Product.objects.all().count()
     context = {
         "orders": orders,
+        "workers_count": workers_count,
+        "orders_count": orders_count,
+        "products_count": products_count,
     }
     return render(request, 'dashboard/order.html', context)
 
