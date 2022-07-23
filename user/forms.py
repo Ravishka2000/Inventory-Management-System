ed83f0a8a851b905c192c17d55af459f171166de
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2022-07-23 14:07:30 +0530

    Configure profile page successfully

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class CreateUserForm(UserCreationForm):
@@ -12,3 +13,15 @@ class CreateUserForm(UserCreationForm):
         model = User
         # set fields of the form
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['address', 'phone', 'image']
